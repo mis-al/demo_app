6b0bbcbf3eb9c6942320d759eafacb2a52e6951d
mis-al <aleksandr.misyura@gmail.com>
2014-08-27 09:44:12 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DomoApp::Application.config.secret_key_base = 'cd372a5e0da7f1c5e16dda0802b4700f67b24b8f94643fd2dd7252fba5a84a411ab7ace6d5314aa19be2722a22fc5c7e6d0dbf3322a5283f815edd52e9bcaf5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DomoApp::Application.config.session_store :cookie_store, key: '_domo_app_session'
